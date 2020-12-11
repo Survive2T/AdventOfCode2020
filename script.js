a2c8bd73d52961e8f66dbf1f383762be83000372
Nicholas Deckard <survive2t@gmail.com>
2020-12-11 02:48:29 -0600

    Get userInput in script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById("submit").addEventListener("click", fixExpenseReport);
+
+function fixExpenseReport() {
+  let userInput = document.querySelector("#userInput").value;
+  console.log(userInput);
+}
