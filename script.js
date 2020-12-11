bd07856b20a58be42c6ee2f088844462e09942cb
Nicholas Deckard <survive2t@gmail.com>
2020-12-11 04:08:16 -0600

    Complete Challenge

--- script.js
+++ script.js
@@ -1,7 +1,35 @@
 document.getElementById("submit").addEventListener("click", fixExpenseReport);
 
 function fixExpenseReport() {
+  let divResult = document.getElementById("result");
   let userInput = document.querySelector("#userInput").value;
   let data = userInput.match(/\d+/g);
-  console.log(data);
+
+  let index = 0;
+  data.forEach((element) => {
+    data[index] = parseInt(element);
+    index++;
+  });
+  let result;
+  data.forEach((i) => {
+    data.forEach((j) => {
+      let sum = i + j;
+      if (sum == 2020) {
+        result = i * j;
+      }
+    });
+  });
+  console.log(result);
+
+  data.forEach((i) => {
+    data.forEach((j) => {
+      data.forEach((k) => {
+        let sum = i + j + k;
+        if (sum == 2020) {
+          result = i * j * k;
+        }
+      });
+    });
+  });
+  console.log(result);
 }
