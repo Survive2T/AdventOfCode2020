2e771f36c3975452ef9a38ebdb9bdb399852e7bb
Nicholas Deckard <survive2t@gmail.com>
2020-12-11 06:03:23 -0600

    Setup Page to Accept and Get User Input

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById("submit").addEventListener("click", GetUserInput);
+
+function GetUserInput() {
+  let divResult = document.getElementById("result");
+  let userInput = document.querySelector("#userInput").value;
+}
