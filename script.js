88e23fcf34fff08b173e65aee6fa1dd07159646a
Nicholas Deckard <survive2t@gmail.com>
2020-12-11 09:11:43 -0600

    Complete Part 2

--- script.js
+++ script.js
@@ -1,20 +1,35 @@
 document.getElementById("submit").addEventListener("click", GetUserInput);
 
+function FindTrees(slopeX, slopeY, map, height) {
+  let count = 0;
+
+  x = slopeX;
+  y = slopeY;
+  while (y < height) {
+    if (map[y][x] == "#") {
+      count++;
+    }
+    x = x + slopeX;
+    y = y + slopeY;
+  }
+  return count;
+}
+
 function GetUserInput() {
   let userInput = document.querySelector("#userInput").value;
   let lineElements = userInput.split("\n");
   let x = 0;
   let y = 0;
   let offsetX;
-  let count = 0;
-
   let map = [];
+  let trees = [];
+
   for (y = 0; y < lineElements.length; y++) {
     map[y] = [];
   }
   for (
     offsetX = 0;
-    offsetX < (lineElements.length * 3) / lineElements[0].length;
+    offsetX < (lineElements.length * 7) / lineElements[0].length;
     offsetX++
   ) {
     for (y = 0; y < lineElements.length; y++) {
@@ -23,15 +38,17 @@ function GetUserInput() {
       }
     }
   }
-  console.log(map);
-  x = 3;
-  y = 1;
-  while (y < lineElements.length) {
-    if (map[y][x] == "#") {
-      count++;
-    }
-    console.log("Trees: " + count);
-    x = x + 3;
-    y = y + 1;
-  }
+  trees[0] = FindTrees(1, 1, map, lineElements.length);
+  trees[1] = FindTrees(3, 1, map, lineElements.length);
+  trees[2] = FindTrees(5, 1, map, lineElements.length);
+  trees[3] = FindTrees(7, 1, map, lineElements.length);
+  trees[4] = FindTrees(1, 2, map, lineElements.length);
+  trees.forEach((element) => {
+    console.log(element);
+  });
+  let treeProduct = 1;
+  trees.forEach((tree) => {
+    treeProduct = treeProduct * tree;
+  });
+  console.log(treeProduct);
 }
